4e278ff33d202c2da4090d944d8e1ca53168fa7c
tvayer <titouan.vayer@gmail.com>
2018-10-14 15:22:46 +0200

    update continuous attributes

--- pscn.py
+++ pscn.py
@@ -1,6 +1,6 @@
 "Patchy San"
 
-import networkx as nx
+from networkx import nx
 from networkx import convert_node_labels_to_integers
 from pynauty.graph import canonical_labeling,NautyGraph
 import copy
@@ -8,106 +8,190 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from keras.models import Model,Sequential
 from keras.layers import Conv1D,Dense,Dropout,Flatten,Activation,Input
 import numpy as np
+import time
+import tensorflow as tf
+import utils
+# your code here
+
 
 class PSCN():
     def __init__(self,w,s=1,k=10,labeling_procedure_name='betweeness'
-                 ,epochs=150,batch_size=25,verbose=0):
-    """
-    w : the length of the node sequence 
-    k : the size of the receptive field
-    labeling_procedure_name : name of the labeling procedure, just implemented betweeness
-    epochs : epochs for the CNN model 
-    batch_size : the batch size for the CNN model
-    """
+                 ,epochs=150,batch_size=25,verbose=0,use_node_deg=False
+                 ,use_preprocess_data=False,gpu=False,multiclass=None,one_hot=0,attr_dim=1,dummy_value=-1):
         self.w=w
         self.s=s
         self.k=k
         self.labeling_procedure_name=labeling_procedure_name
         self.epochs=epochs
+        self.use_node_deg=use_node_deg
         self.batch_size=batch_size
         self.verbose=verbose
+        self.use_preprocess_data=use_preprocess_data
+        self.gpu=gpu
+        self.multiclass=multiclass
+        self.one_hot=one_hot
+        self.attr_dim=attr_dim
+        self.dummy_value=dummy_value
         self.model = KerasClassifier(build_fn=self.create_model
                                      ,epochs=self.epochs, 
                                      batch_size=self.batch_size, verbose=self.verbose)
+        self.times_process_details={}
+        self.times_process_details['normalized_subgraph']=[]
+        self.times_process_details['neigh_assembly']=[]
+        self.times_process_details['canonicalizes']=[]
+        self.times_process_details['compute_subgraph_ranking']=[]
+        self.times_process_details['labeling_procedure']=[]       
+        self.times_process_details['first_labeling_procedure']=[]
+        
+        if self.one_hot>0:
+            self.attr_dim=self.one_hot
 
         
     def create_model(self):
-        # just binary for now
        model=Sequential()
-       model.add(Conv1D(filters=16,kernel_size=self.k,strides=self.k,input_shape=(self.w*self.k,1)))
+       model.add(Conv1D(filters=16,kernel_size=self.k,strides=self.k,input_shape=(self.w*self.k,self.attr_dim)))    
        model.add(Conv1D(filters=8,kernel_size=10,strides=1))
        model.add(Flatten())
-       model.add(Dense(128,activation="relu"))
+       model.add(Dense(128,activation="relu",name='embedding_layer'))
        model.add(Dropout(0.5))
-       model.add(Dense(1,activation="sigmoid"))
-       model.compile(loss="binary_crossentropy",optimizer="rmsprop",metrics=["accuracy"])    
+       if self.multiclass is not None :
+           model.add(Dense(self.multiclass, activation='softmax'))
+           model.compile(loss="categorical_crossentropy",optimizer="rmsprop",metrics=["accuracy"])
+       else:
+           model.add(Dense(1,activation="sigmoid"))
+           model.compile(loss="binary_crossentropy",optimizer="rmsprop",metrics=["accuracy"])    
        return model
    
     def process_data(self,X,y=None):
-        """
-        X is a list of networkx graphs
-        y is a list of class 
-        """
+        start=time.time()
         n=len(X)
         train=[]
         for i in range(n):
-           rfMaker=ReceptiveFieldMaker(X[i],w=self.w
-                                       ,k=self.k,s=self.s
-                                       ,labeling_procedure_name=self.labeling_procedure_name)
-           forcnn=rfMaker.make_()
-           train.append(np.array(forcnn).flatten().reshape(self.k*self.w,1))
-           
+            rfMaker=ReceptiveFieldMaker(X[i].nx_graph,w=self.w,k=self.k,s=self.s
+                                        ,labeling_procedure_name=self.labeling_procedure_name
+                                        ,use_node_deg=self.use_node_deg,one_hot=self.one_hot,dummy_value=self.dummy_value)
+            forcnn=rfMaker.make_()
+            self.times_process_details['neigh_assembly'].append(np.sum(rfMaker.all_times['neigh_assembly']))
+            self.times_process_details['normalized_subgraph'].append(np.sum(rfMaker.all_times['normalized_subgraph']))
+            self.times_process_details['canonicalizes'].append(np.sum(rfMaker.all_times['canonicalizes']))
+            self.times_process_details['compute_subgraph_ranking'].append(np.sum(rfMaker.all_times['compute_subgraph_ranking']))
+            self.times_process_details['labeling_procedure'].append(np.sum(rfMaker.all_times['labeling_procedure']))
+            self.times_process_details['first_labeling_procedure'].append(np.sum(rfMaker.all_times['first_labeling_procedure']))
+            
+            train.append(np.array(forcnn).flatten().reshape(self.k*self.w,self.attr_dim))
+
         X_preprocessed=np.array(train)
+        end=time.time()
+        print('Time preprocess data in s',end-start) 
         if y is not None:
             y_preprocessed=[y[i] for i in range(n)]
-        
             return X_preprocessed,y_preprocessed
         else :
             return X_preprocessed
    
     def fit(self,X,y=None):
-        """
-        X is a list of networkx graphs
-        y is a list of class 
-        """
-        X_preprocessed,y_preprocessed=self.process_data(X,y)
-        self.model.fit(X_preprocessed,y_preprocessed)
+        if not self.use_preprocess_data:
+            X_preprocessed,y_preprocessed=self.process_data(X,y)
+        else:
+            X_preprocessed=X
+            y_preprocessed=y
+        start=time.time()
+        if self.gpu:
+            with tf.device('/gpu:0'):
+                if self.verbose >0:
+                    print('Go for GPU')
+                self.model.fit(X_preprocessed,y_preprocessed)
+        else:
+            self.model.fit(X_preprocessed,y_preprocessed)
+        end=time.time()
+        print('Time fit data in s',end-start)
         
     def predict(self,X):
-        X_preprocessed=self.process_data(X)
+        if not self.use_preprocess_data:
+            X_preprocessed=self.process_data(X)
+        else:
+            X_preprocessed=X
         return self.model.predict(X_preprocessed)
+    
+    def return_embedding(self,X):
+        X_preprocessed=self.process_data(X)
+        layer_output = Model(inputs=self.model.model.input,
+                                 outputs=self.model.model.get_layer('embedding_layer').output)        
+        return layer_output.predict(X_preprocessed)
 
 
 
 class ReceptiveFieldMaker():
-    """
-    nx_graph: a networkx graph. A receptive field is created over this graph
-    w : the length of the node sequence 
-    k : the size of the receptive field
-    labeling_procedure_name : name of the labeling procedure, just implemented betweeness
-    """
-    def __init__(self,nx_graph,w,s=1,k=10,labeling_procedure_name='betweeness'):
+    def __init__(self,nx_graph,w,s=1,k=10,labeling_procedure_name='betweeness',use_node_deg=False,one_hot=False,dummy_value=-1):
         self.nx_graph=nx_graph
+        self.use_node_deg=use_node_deg
+        if self.use_node_deg:
+            node_degree_dict=dict(self.nx_graph.degree())
+            normalized_node_degree_dict={k:v/len(self.nx_graph.nodes()) for k,v in node_degree_dict.items() }
+            nx.set_node_attributes(self.nx_graph,normalized_node_degree_dict,'attr_name')
+        self.all_times={}
+        self.all_times['neigh_assembly']=[]
+        self.all_times['normalized_subgraph']=[]
+        self.all_times['canonicalizes']=[]   
+        self.all_times['compute_subgraph_ranking']=[]
+        self.all_times['labeling_procedure']=[]
+        self.all_times['first_labeling_procedure']=[]
         self.w=w
         self.s=s
         self.k=k
+        self.dummy_value=dummy_value
+        self.exists_dummies=False
+        self.one_hot=one_hot
         self.labeling_procedure_name=labeling_procedure_name
-        self.dict_first_labeling=self.labeling_procedure(self.nx_graph)
+        
+        if self.labeling_procedure_name=='approx_betweeness':
+            st=time.time()
+            self.dict_first_labeling=self.betweenness_centrality_labeling(self.nx_graph,approx=int(len(self.nx_graph.nodes())/5)+1) 
+            self.labeling_procedure_name='betweeness'
+            end=time.time()
+            self.all_times['first_labeling_procedure'].append(end-st)
+        elif self.labeling_procedure_name=='betweeness':
+            st=time.time()
+            self.dict_first_labeling=self.betweenness_centrality_labeling(self.nx_graph) 
+            end=time.time()
+            self.all_times['first_labeling_procedure'].append(end-st)   
+        else :
+            st=time.time()
+            self.dict_first_labeling=self.labeling_procedure(self.nx_graph) 
+            end=time.time()
+            self.all_times['first_labeling_procedure'].append(end-st)    
+            
         self.original_labeled_graph=self.dict_first_labeling['labeled_graph']
+
+
 
     def make_(self):
         "Result on one (w,k,length_attri) list (usually (w,k,1)) for 1D CNN "
         forcnn=[]
         f=self.select_node_sequence()
         for graph in f :
-            frelabel=nx.relabel_nodes(graph,nx.get_node_attributes(graph,'labeling'))
-            forcnn.append([x[1] for x in sorted(nx.get_node_attributes(frelabel,'attr_name').items(),key=lambda x:x[0])])
+            frelabel=nx.relabel_nodes(graph,nx.get_node_attributes(graph,'labeling')) #rename the nodes wrt the labeling
+            if self.one_hot>0:
+                forcnn.append([utils.indices_to_one_hot(x[1],self.one_hot) for x in sorted(nx.get_node_attributes(frelabel,'attr_name').items(),key=lambda x:x[0])])
+            else:
+                forcnn.append([x[1] for x in sorted(nx.get_node_attributes(frelabel,'attr_name').items(),key=lambda x:x[0])])
         return forcnn
-
-    def betweenness_centrality_labeling(self,graph):
+    
+    def labeling_procedure(self,graph):   
+        st=time.time()
+        if self.labeling_procedure_name=='betweeness':
+            a=self.betweenness_centrality_labeling(graph)
+        end=time.time()
+        self.all_times['labeling_procedure'].append(end-st)
+        return a
+    
+    def betweenness_centrality_labeling(self,graph,approx=None):
         result={}
         labeled_graph=nx.Graph(graph)
-        centrality=list(nx.betweenness_centrality(graph).items())
+        if approx is None:
+            centrality=list(nx.betweenness_centrality(graph).items())
+        else:
+            centrality=list(nx.betweenness_centrality(graph,k=approx).items())
         sorted_centrality=sorted(centrality,key=lambda n:n[1],reverse=True)
         dict_={}
         label=0
@@ -188,9 +272,7 @@ class ReceptiveFieldMaker():
 
         return result
 
-    def labeling_procedure(self,graph):
-        if self.labeling_procedure_name=='betweeness':
-            return self.betweenness_centrality_labeling(graph)
+
 
     def select_node_sequence(self):
         Vsort=self.dict_first_labeling['ordered_nodes']
@@ -198,7 +280,6 @@ class ReceptiveFieldMaker():
         i=0
         j=1
         while j<=self.w :
-
             if i<len(Vsort):
                 f.append(self.receptiveField(Vsort[i]))
             else:
@@ -210,20 +291,26 @@ class ReceptiveFieldMaker():
 
     def zeroReceptiveField(self):
         graph=nx.star_graph(self.k-1) #random graph peu importe sa tete
-        nx.set_node_attributes(graph,0,'attr_name')
+        nx.set_node_attributes(graph,self.dummy_value,'attr_name')
         nx.set_node_attributes(graph,{k:k for k,v in dict(graph.nodes()).items()},'labeling')
 
         return graph
 
     def receptiveField(self,vertex):
+        st=time.time()
         subgraph=self.neighborhood_assembly(vertex)
+        ed=time.time()
+        self.all_times['neigh_assembly'].append(ed-st)
         normalized_subgraph=self.normalize_graph(subgraph,vertex)
+        ed2=time.time()
+        self.all_times['normalized_subgraph'].append(ed2-ed)
+
 
         return normalized_subgraph
 
 
     def neighborhood_assembly(self,vertex):
-        "Output networkx subgraph"
+        "Output a set of neighbours of the vertex"
         N={vertex}
         L={vertex}
         while len(N)<self.k and len(L)>0:
@@ -264,6 +351,8 @@ class ReceptiveFieldMaker():
         return new_ordered_dict
 
     def compute_subgraph_ranking(self,subgraph,vertex,original_order_to_respect):
+        
+        st=time.time()
 
         labeled_graph=nx.Graph(subgraph)
         ordered_subgraph_from_centrality=self.labeling_to_root(subgraph,vertex)
@@ -273,10 +362,13 @@ class ReceptiveFieldMaker():
         new_ordered_dict=self.rank_label_wrt_dict(ordered_subgraph_from_centrality,all_labels_in_subgraph_dict,original_order_to_respect)
 
         nx.set_node_attributes(labeled_graph,new_ordered_dict,'labeling') 
-
+        ed=time.time()
+        self.all_times['compute_subgraph_ranking'].append(ed-st)
         return labeled_graph
 
     def canonicalizes(self,subgraph):
+        
+        st=time.time()
 
         #wl_subgraph_normalized=self.wl_normalization(subgraph)['labeled_graph']
         #g_relabel=convert_node_labels_to_integers(wl_subgraph_normalized)
@@ -293,6 +385,9 @@ class ReceptiveFieldMaker():
         new_ordered_dict=self.rank_label_wrt_dict(g_relabel,labels_dict,canonical_labeling_dict)
 
         nx.set_node_attributes(labeled_graph,new_ordered_dict,'labeling') 
+        
+        ed=time.time()
+        self.all_times['canonicalizes'].append(ed-st)
 
         return labeled_graph
 
@@ -314,20 +409,23 @@ class ReceptiveFieldMaker():
             subgraph_ranked_N=self.compute_subgraph_ranking(subgraph_N,vertex,original_order_to_respect)
 
         elif len(subgraph_U.nodes())<self.k:
-            print('shit could happen') #TODO : ajouter des dummy nodes
-            N=U
-            N.append(dummy_nodes(k-len(U)))
+            subgraph_ranked_N=self.add_dummy_nodes_at_the_end(subgraph_U)
         else :
             subgraph_ranked_N=subgraph_U
 
         return self.canonicalizes(subgraph_ranked_N)
 
-    def neighborhood_assembly_list(self,sequence,graph,k):
-        graph_list=[]
-        for v in sequence:
-            nodes_of_interest=neighborhood_assembly(graph,v,k)
-            graph_list.append(graph.subgraph(nodes_of_interest))
-        return graph_list
+    def add_dummy_nodes_at_the_end(self,nx_graph): #why 0 ??
+        self.exists_dummies=True
+        g=nx.Graph(nx_graph)
+        keys=[k for k,v in dict(nx_graph.nodes()).items()]
+        labels=[v for k,v in dict(nx.get_node_attributes(nx_graph,'labeling')).items()]
+        j=1
+        while len(g.nodes())<self.k:
+            g.add_node(max(keys)+j,attr_name=self.dummy_value,labeling=max(labels)+j)
+            j+=1
+        return g
+            
 
     def labeling_to_root(self,graph,vertex):
         labeled_graph=nx.Graph(graph)
